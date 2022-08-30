f214fed1b4ca90cf7bb12724ba446890905e991d
alfredo <alfredo67316@gmail.com>
2022-08-30 20:35:47 +0700

    menambah file

---
+++ script.js
@@ -0,0 +1,10 @@
+const heroBg = document.querySelector('.hero');
+let tukar = 1;
+
+setInterval(() => {
+    heroBg.style.backgroundImage = `url(/img/bg/bg-${tukar}.jpg)`;
+    tukar += 1
+    if(tukar >= 4){
+        tukar = 1
+    }
+},3000)
