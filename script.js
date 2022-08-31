3bd32d119e71b084cb60be48586b19c970beb1c1
alfredo <alfredo67316@gmail.com>
2022-08-31 17:34:43 +0700

    mengubah file js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heroBg = document.querySelector('.hero');
 let tukar = 1;
 
 setInterval(() => {
-    heroBg.style.backgroundImage = `url(/img/bg/bg-${tukar}.jpg)`;
+    heroBg.style.backgroundImage = `url(img/bg/bg-${tukar}.jpg)`;
     tukar += 1
     if(tukar >= 4){
         tukar = 1
